5400d13fbbc167ded03040deb59101be96bcf7c3
wangshengwen <wswen1220@163.com>
2017-04-11 15:09:00 +0800

    freenasUI

--- freenasUI/storage/views.py
+++ freenasUI/storage/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'storage/index.html')
+def pool(request):
+    return render(request,'storage/pool.html')
